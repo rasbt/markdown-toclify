4ae5ec1a84166abf4f79a6aa9f40d165edd2d68e
rasbt <se.raschka@me.com>
2014-06-11 01:20:46 -0400

    little code tweak using .count()

--- markdown-toclify.py
+++ markdown-toclify.py
@@ -62,13 +62,14 @@ def dashify_headline(line):
 
     """
     stripped_right = line.rstrip('#')
+    level = stripped_right.count('#')
     stripped_both = stripped_right.lstrip('#')
-    level = len(stripped_right) - len(stripped_both)
     stripped_wspace = stripped_both.strip()
     dashified = "-".join(stripped_wspace.split(' '))
     if level > 6:   # HTML supports headlines only up to <h6>
         level = 6
     return stripped_wspace, dashified, level
+
 
 def get_lines(in_filename):
     """
