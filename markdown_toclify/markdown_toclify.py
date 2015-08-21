8398edfc00ec102e5cab84f7a4140cb9ac7638ad
rasbt <mail@sebastianraschka.com>
2015-08-21 12:32:58 -0400

    pep 8 compliance

--- markdown_toclify/markdown_toclify.py
+++ markdown_toclify/markdown_toclify.py
@@ -29,6 +29,7 @@ __version__ = '1.7.0'
 
 VALIDS = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-&'
 
+
 def read_lines(in_file):
     """Returns a list of lines from a input markdown file."""
 
@@ -71,17 +72,17 @@ def dashify_headline(line):
     # character replacements
     replaced_colon = stripped_wspace.replace('.', '')
     replaced_slash = replaced_colon.replace('/', '')
-    rem_nonvalids = ''.join([c if c in VALIDS else '-' for c in replaced_slash])
+    rem_nonvalids = ''.join([c if c in VALIDS
+                             else '-' for c in replaced_slash])
 
     lowered = rem_nonvalids.lower()
-    dashified = re.sub(r'(-)\1+', r'\1', lowered) # remove duplicate dashes
-    dashified = dashified.strip('-') # strip dashes from start and end
+    dashified = re.sub(r'(-)\1+', r'\1', lowered)  # remove duplicate dashes
+    dashified = dashified.strip('-')  # strip dashes from start and end
 
     # exception '&' (double-dash in github)
     dashified = dashified.replace('-&-', '--')
 
     return [stripped_wspace, dashified, level]
-
 
 
 def tag_and_collect(lines, id_tag=True, back_links=False, exclude_h=None):
@@ -127,7 +128,7 @@ def tag_and_collect(lines, id_tag=True, back_links=False, exclude_h=None):
             if not l.lstrip('#').startswith(' '):
                 continue
             # not a headline if more than 6 '#':
-            if len(l) - len(l.lstrip('#')) > 6  :
+            if len(l) - len(l.lstrip('#')) > 6:
                 continue
             # headers can be indented by at most 3 spaces:
             if orig_len - len(l) > 3:
@@ -142,7 +143,8 @@ def tag_and_collect(lines, id_tag=True, back_links=False, exclude_h=None):
 
             if not exclude_h or not dashified[-1] in exclude_h:
                 if id_tag:
-                    id_tag = '<a class="mk-toclify" id="%s"></a>' %(dashified[1])
+                    id_tag = '<a class="mk-toclify" id="%s"></a>'\
+                              % (dashified[1])
                     out_contents.append(id_tag)
                 headlines.append(dashified)
 
@@ -154,7 +156,7 @@ def tag_and_collect(lines, id_tag=True, back_links=False, exclude_h=None):
 
 def positioning_headlines(headlines):
     """
-    Strips unnecessary whitespaces/tabs if the first header is not left-aligned.
+    Strips unnecessary whitespaces/tabs if first header is not left-aligned
     """
     left_just = False
     for row in headlines:
@@ -194,13 +196,12 @@ def create_toc(headlines, hyperlink=True, top_link=False, no_toc_header=False):
 
     for line in headlines:
         if hyperlink:
-            item = '%s- [%s](#%s)' %((line[2]-1)*'    ', line[0], line[1])
+            item = '%s- [%s](#%s)' % ((line[2]-1)*'    ', line[0], line[1])
         else:
-            item = '%s- %s' %((line[2]-1)*'    ', line[0])
+            item = '%s- %s' % ((line[2]-1)*'    ', line[0])
         processed.append(item)
     processed.append('\n')
     return processed
-
 
 
 def build_markdown(toc_headlines, body, spacer=0, placeholder=None):
@@ -222,7 +223,7 @@ def build_markdown(toc_headlines, body, spacer=0, placeholder=None):
 
     """
     if spacer:
-        spacer_line = ['\n<div style="height:%spx;"></div>\n' %(spacer)]
+        spacer_line = ['\n<div style="height:%spx;"></div>\n' % (spacer)]
         toc_markdown = "\n".join(toc_headlines + spacer_line)
     else:
         toc_markdown = "\n".join(toc_headlines)
@@ -248,10 +249,10 @@ def output_markdown(markdown_cont, output_file):
 
 
 def markdown_toclify(input_file, output_file=None, github=False,
-                    back_to_top=False, nolink=False,
-                    no_toc_header=False, spacer=0, placeholder=None,
-                    exclude_h=None):
-    ''' Function to add table of contents to markdown files.
+                     back_to_top=False, nolink=False,
+                     no_toc_header=False, spacer=0, placeholder=None,
+                     exclude_h=None):
+    """ Function to add table of contents to markdown files.
 
     Parameters
     -----------
@@ -289,7 +290,7 @@ def markdown_toclify(input_file, output_file=None, github=False,
     cont: str
       Markdown contents including the TOC.
 
-    '''
+    """
     raw_contents = read_lines(input_file)
     cleaned_contents = remove_lines(raw_contents, remove=('[[back to top]', '<a class="mk-toclify"'))
     processed_contents, raw_headlines = tag_and_collect(
@@ -301,9 +302,9 @@ def markdown_toclify(input_file, output_file=None, github=False,
 
     leftjustified_headlines = positioning_headlines(raw_headlines)
     processed_headlines = create_toc(leftjustified_headlines,
-                                        hyperlink=not nolink,
-                                        top_link=not nolink and not github,
-                                        no_toc_header=no_toc_header)
+                                     hyperlink=not nolink,
+                                     top_link=not nolink and not github,
+                                     no_toc_header=no_toc_header)
 
     if nolink:
         processed_contents = cleaned_contents
@@ -337,53 +338,43 @@ def commandline():
     https://github.com/rasbt/markdown-toclify
 
     """,
-        formatter_class=argparse.RawTextHelpFormatter
+            formatter_class=argparse.RawTextHelpFormatter
     )
 
     parser.add_argument('InputFile',
-            metavar='input.md',
-            help='path to the Markdown input file'
-            )
+                        metavar='input.md',
+                        help='path to the Markdown input file')
     parser.add_argument('-o', '--output',
-            metavar='output.md',
-            default=None,
-            help='path to the Markdown output file'
-            )
+                        metavar='output.md',
+                        default=None,
+                        help='path to the Markdown output file')
     parser.add_argument('-b', '--back_to_top',
-            action='store_true',
-            help='add [back to top] links.'
-            )
+                        action='store_true',
+                        help='add [back to top] links.')
     parser.add_argument('-g', '--github',
-            action='store_true',
-            help='omits id-anchor tags (recommended for GitHub)'
-            )
+                        action='store_true',
+                        help='omits id-anchor tags (recommended for GitHub)')
     parser.add_argument('-s', '--spacer',
-            default=0,
-            type=int,
-            metavar='pixels',
-            help='add horizontal space (in pixels) after the table of contents'
-            )
+                        default=0,
+                        type=int,
+                        metavar='pixels',
+                        help='add horizontal space (in pixels) after the table of contents')
     parser.add_argument('-n', '--nolink',
-            action='store_true',
-            help='create the table of contents without internal links'
-            )
+                        action='store_true',
+                        help='create the table of contents without internal links')
     parser.add_argument('-e', '--exclude_h',
-            type=str,
-            default='',
-            help='exclude eading levels, e.g., "2,3" to exclude all level 2 and 3 headings'
-            )
+                        type=str,
+                        default='',
+                        help='exclude eading levels, e.g., "2,3" to exclude all level 2 and 3 headings')
     parser.add_argument('--placeholder',
-            type=str,
-            help='inserts TOC at the placeholder string instead of inserting it on top of the document'
-            )
+                        type=str,
+                        help='inserts TOC at the placeholder string instead of inserting it on top of the document')
     parser.add_argument('--no_toc_header',
-            action='store_true',
-            help='suppresses the Table of Contents header'
-            )
+                        action='store_true',
+                        help='suppresses the Table of Contents header')
     parser.add_argument('-v', '--version',
-            action='version',
-            version='%s' %__version__
-            )
+                        action='version',
+                        version='%s' % __version__)
 
     args = parser.parse_args()
 
@@ -393,14 +384,14 @@ def commandline():
         exclude_h = None
 
     cont = markdown_toclify(input_file=args.InputFile,
-                     output_file=args.output,
-                     github=args.github,
-                     back_to_top=args.back_to_top,
-                     nolink=args.nolink,
-                     no_toc_header=args.no_toc_header,
-                     spacer=args.spacer,
-                     placeholder=args.placeholder,
-                     exclude_h=exclude_h)
+                            output_file=args.output,
+                            github=args.github,
+                            back_to_top=args.back_to_top,
+                            nolink=args.nolink,
+                            no_toc_header=args.no_toc_header,
+                            spacer=args.spacer,
+                            placeholder=args.placeholder,
+                            exclude_h=exclude_h)
 
     if not args.output:
         print(cont)
