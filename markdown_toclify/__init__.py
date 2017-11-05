9da86e4ed0d6c03c03236a5a38fe368aa4cab4f9
CJ Harries <cj@wizardsoftheweb.pro>
2017-11-05 12:43:08 -0600

    Refactor import

--- markdown_toclify/__init__.py
+++ markdown_toclify/__init__.py
@@ -5,7 +5,7 @@ from .markdown_toclify import markdown_toclify
 from .markdown_toclify import tag_and_collect
 from .markdown_toclify import create_toc
 from .markdown_toclify import positioning_headlines
-from .markdown_toclify import dashify_headline
+from .markdown_toclify import slugify_headline
 from .markdown_toclify import remove_lines
 
 __version__ = '0.1.7'
